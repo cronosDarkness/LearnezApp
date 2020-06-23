14754cacc164ef54de8956d421992c6f3845eca6
alanlb <alanlb195@gmail.com>
2020-06-22 21:05:18 -0500

    home page with routing

--- src/app/pages/home/home.page.ts
+++ src/app/pages/home/home.page.ts
@@ -1,15 +1,46 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: './home.page.html',
-  styleUrls: ['./home.page.scss'],
+  selector: "app-home",
+  templateUrl: "./home.page.html",
+  styleUrls: ["./home.page.scss"],
 })
 export class HomePage implements OnInit {
+  grade = [
+    {
+      grade: "1",
+      color: "#42AE18"
+    },
+    {
+      grade: "2",
+      color: "#F39317"
+    },
+    {
+      grade: "3",
+      color: "#8D33AA"
+    },
+    {
+      grade: "4",
+      color: "#D74726"
+    },
+    {
+      grade: "5",
+      color: "#00A1DE"
+    },
+    {
+      grade: "6",
+      color: "#00B971"
+    },
+  ];
 
-  constructor() { }
+  constructor(
+    public router: Router
+    ) { }
 
-  ngOnInit() {
+  verLibrosPorGrado(grado: string) {
+    this.router.navigate(["/libros", parseInt(grado)]);
   }
 
+  ngOnInit() {}
 }
