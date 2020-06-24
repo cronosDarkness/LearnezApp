f62541e4c64cf2aad0a0c89c86a6c3070e9c0be9
alanlb <alanlb195@gmail.com>
2020-06-24 01:44:37 -0500

    opcion - tema oscuro añadida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
+import { ThemeService } from './shared/services/theme.service';
 
 @Component({
   selector: 'app-root',
@@ -21,12 +22,14 @@ export class AppComponent {
       url: '/home',
       icon: 'clipboard'
     }
-  ]
+  ];
+  theme: boolean = this.themeService.getTheme();
 
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    public themeService: ThemeService,
   ) {
     this.initializeApp();
   }
@@ -35,6 +38,10 @@ export class AppComponent {
     this.platform.ready().then(() => {
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+
+      this.themeService.setTheme();
     });
   }
+
+
 }
