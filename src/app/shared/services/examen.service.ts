782a1747f27d40466bc92fea47c9112064306d7b
cronosDarkness <jtcastro98@gmail.com>
2020-07-19 18:24:33 -0500

    Examen

---
+++ src/app/shared/services/examen.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Examen } from '../models/examen.model';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ExamenService
+{
+
+  constructor(public http: HttpClient) { }
+
+  url = "http://localhost:6045/api/examenes/bloque/";
+
+  getExamen(bloqueId: number): Observable<Examen[]> {
+    return this.http.get<Examen[]>(this.url + bloqueId);
+    
+  }
+
+}
